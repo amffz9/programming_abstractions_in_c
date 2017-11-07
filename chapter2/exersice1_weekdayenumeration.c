6aa8047e7d65d22ce809ef810e3737e9ce196c7c
aforbis <aforbis83091@gmail.com>
2017-11-07 14:34:30 -0600

    Initial commit

---
+++ chapter2/exersice1_weekdayenumeration.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+typedef enum {
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+} weekdayT;
+
+weekdayT IncrementDay(weekdayT startDay, int delta) {
+
+    if (startDay == Saturday && delta > 0) {
+        return Sunday;
+    } else if (startDay == Sunday && delta < 0) {
+        return Saturday;
+    } else {
+        return startDay + delta;
+    }
+}
+
+weekdayT NextDay(weekdayT day) {
+    return IncrementDay(day, 1); //plus one for the next day
+}
+
+weekdayT PreviousDay(weekdayT day) {
+    return IncrementDay(day, -1); //minus one for the previous day
+}
+
+int main(int argc, char const *argv[]) {
+
+    int last_input = 0;
+    weekdayT currentDay = Sunday;
+
+    while (1) {
+        printf("Enter 1 to increase the day by one\n And 2 to decrease the day: \n");
+        scanf("%d", &last_input);
+
+        if (last_input == 1) {
+            currentDay = NextDay(currentDay);
+        }
+        if (last_input == 2) {
+            currentDay = PreviousDay(currentDay);
+        }
+
+        if (last_input == -1) {
+            break;
+        }
+
+        switch (currentDay) {
+            case Sunday:
+                printf("Sunday\n");
+                break;
+            case Monday:
+                printf("Monday\n");
+                break;
+            case Tuesday:
+                printf("Tuesday\n");
+                break;
+            case Wednesday:
+                printf("Wednesday\n");
+                break;
+            case Thursday:
+                printf("Thursday\n");
+                break;
+            case Friday:
+                printf("Friday\n");
+                break;
+            case Saturday:
+                printf("Saturday\n");
+                break;
+            default:
+                printf("Invalid Day\n");
+                break;
+        }
+    }
+
+
+    return 0;
+}
